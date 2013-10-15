1788dac3340cde53ec432e0108b99a26bcf5754f
Folkol <mattias4@kth.se>
2013-10-15 17:46:50 +0200

    Completed display tutorial

---
+++ src/com/folkol/tutorial/lwjgl/TheDisplay.java
@@ -0,0 +1,22 @@
+package com.folkol.tutorial.lwjgl;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class TheDisplay {
+    public static void main(String[] argv) throws LWJGLException {
+        DisplayMode[] availableDisplayModes = Display.getAvailableDisplayModes();
+        Display.setFullscreen(true);
+        for (DisplayMode dm : availableDisplayModes) {
+            System.out.println(dm);
+            Display.setDisplayMode(dm);
+            Display.create();
+            while (!Display.isCloseRequested()) {
+                // Render something
+                Display.update();
+            }
+            Display.destroy();
+        }
+    }
+}
